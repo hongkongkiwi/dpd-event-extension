097241f23f4c9233cd9a46c701a813a55deea9a1
Andy <andy@savage.hk>
2015-05-08 16:07:38 +0800

    Updated to include underscore and a new function isRole used for checking if a user has a particular role

--- event_extension.js
+++ event_extension.js
@@ -1,6 +1,9 @@
 (function(Script) {
+  var _ = require('underscore')._;
   var _run = Script.prototype.run;
   Script.prototype.run = function(ctx, domain, fn) {
+    var me = ctx.session && ctx.session.user;
+
     if (typeof domain === 'object') {
       domain.process = function() { // access process via process()
         return process;
@@ -10,6 +13,15 @@
       };
       domain.context = function() { // access Context via context()
         return ctx;
+      };
+      domain._ = function() {
+        return _;
+      };
+      domain.isRole = function(r) {
+        if (me.hasOwnProperty('roles') && typeof me.roles === Array) {
+          return false;
+        }
+        return _.contains(me.roles, r);
       };
     }
     _run.call(this, ctx, domain, fn);
